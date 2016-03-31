8e271ba0a36eba9b02798fd8b9a62020d8e73675
Harley Trung <harley@bsa.edu.vn>
2016-03-31 20:07:06 +0700

    allow guests to see chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     respond_to do |format|
